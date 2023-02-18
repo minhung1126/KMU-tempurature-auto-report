dd234dac0f5513d81564912759269c3a0c2d809f
Min Hung DeskMeet B660 <minhung1126@gmail.com>
2023-02-18 14:58:54 +0800

    Add chrome option

--- main.py
+++ main.py
@@ -53,7 +53,11 @@ def main():
 
     # driver = webdriver.Chrome(service=ChromiumService(
     #     ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install()))
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.implicitly_wait(30)
 
